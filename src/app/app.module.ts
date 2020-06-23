8f64534a668e583e8d0daa4955e434910287cee2
Jonatas <jonatas.coelho@aluno.faculdadeimpacta.com.br>
2020-06-23 19:57:05 -0300

    menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { MenuComponent } from './menu/menu.component'
 
 @NgModule({
   imports:      [ BrowserModule ], //modulos usados na aplicação
-  declarations: [ AppComponent ], // componentes usados na aplicação
-  bootstrap:    [ AppComponent ] // incica qual componente inicial da aplicação
+  declarations: [ AppComponent, MenuComponent ], // componentes usados na aplicação
+  bootstrap:    [ AppComponent, MenuComponent ] // incica qual componente inicial da aplicação
 })
 export class AppModule { }
