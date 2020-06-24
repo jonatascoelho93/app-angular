ae7b65318b3ecc3c33e262a71a2dc3b375791be0
Jonatas <jonatas.coelho@aluno.faculdadeimpacta.com.br>
2020-06-24 18:57:23 -0300

    view de formulario criada

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -2,11 +2,8 @@ import { Component } from '@angular/core';
 import { ICurso } from '../classes/interface.curso';
 
 @Component({
-    template: `
-    <div class="container margem">
-        <h1>Cadastro de curso</h1>
-    </div>
-    `
+    moduleId: module.id,
+    templateUrl: 'views/cadastro.component.html'
 })
 
 export class CadastroComponent {
